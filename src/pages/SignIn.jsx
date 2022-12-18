31f1d7bcd2e3670afd77e2835bdbedfe27f13ff5
user <palmtree8880@gmail.com>
2022-12-18 12:41:10 +0900

    サインイン

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -40,7 +40,7 @@ export default function SignIn() {
 
   return (
     <section>
-      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+      <h1 className="text-3xl text-center mt-6 font-bold">サインイン</h1>
       <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
         <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
           <img src={keyImgURL} alt="key" className="w-full rounded-2xl"/>
@@ -67,16 +67,16 @@ export default function SignIn() {
             {showPassword ? (<AiFillEyeInvisible className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=>setShowPassword((prevState)=>!prevState)}/>) : (<AiFillEye className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=>setShowPassword((prevState)=>!prevState)}/>) }
             </div>
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg" >
-              <p className='mb-6'>Don't have a account?
-                <Link to="/sign-up" className='text-red-600 hover:text-red-800 transition duration-200 ease-in-out ml-1'>Register</Link>
+              <p className='mb-6'>アカウントをお持ちですか？
+                <Link to="/sign-up" className='text-red-600 hover:text-red-800 transition duration-200 ease-in-out ml-1'>アカウント登録する</Link>
               </p>
               <p>
-                <Link to="/forgot-password" className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out'>Forgot password?</Link>
+                <Link to="/forgot-password" className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out'>パスワードを忘れましたか?</Link>
               </p>
             </div>
-            <button className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800' type="submit">Sign in</button>
+            <button className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800' type="submit">サインイン</button>
             <div className='flex items-center my-4 before:border-t  before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300'>
-              <p className='text-center font-semibold mx-4'>OR</p>
+              <p className='text-center font-semibold mx-4'>または</p>
             </div>
             <OAuth />
           </form>
